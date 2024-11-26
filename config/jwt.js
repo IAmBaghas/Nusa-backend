744aff7c9e7f9b3f3fb8c790ca0b7700118dba66
Aryo Bagas Sasongko <106600608+IAmBaghas@users.noreply.github.com>
2024-11-26 14:33:25 +0700

    Hapus JWT

--- config/jwt.js
+++ config/jwt.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    secret: process.env.JWT_SECRET || 'f0bdf7c5fe951e632ea788a874a78574b8ff100bb1c3e0a347041a98ae25dfb7c2e4986da917df5db6baf0d84c194085de195bba8441b33a61e0c383390bd5aa',
+    secret: process.env.JWT_SECRET || 'YOUR_JWT_SECRET',
     expiresIn: '2h'
 };
